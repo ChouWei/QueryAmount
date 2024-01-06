5950ae0c1dd2978c99e9c574a942fbded21d2960
DavidChou <david9377749097@gmail.com>
2024-01-06 15:28:39 +0800

    feat. add unit test

--- SearchAmount/SearchAmount/Service/BudgetService.cs
+++ SearchAmount/SearchAmount/Service/BudgetService.cs
@@ -1,10 +1,36 @@
+using SearchAmount.Repository;
+
 namespace SearchAmount.Service;
 
 public class BudgetService
 {
+    private readonly IBudgetRepo _budgetRepo;
+
+    public BudgetService(IBudgetRepo budgetRepo)
+    {
+        _budgetRepo = budgetRepo;
+    }
+
     public decimal Query(DateTime start, DateTime end)
     {
-        
+        var result = _budgetRepo.GetAll();
+        var startDate = start.ToString("yyyyMM");
+        var endDate = end.ToString("yyyyMM");
+
+        if (start.CompareTo(end).Equals(0))
+        {
+            var oneMonthAmount = result.Where(i => i.YearMonth.Equals(startDate)).Select(i => i.Amount)
+                .FirstOrDefault();
+            var daysInMonth = DateTime.DaysInMonth(start.Year, start.Month);
+            return oneMonthAmount / daysInMonth;
+        }
+
+        if (startDate.Equals(endDate))
+        {
+            return result.Where(i => i.YearMonth.Equals(startDate)).Select(i => i.Amount)
+                .FirstOrDefault();
+        }
+
         return 0;
     }
 }
--- SearchAmount/SearchAmountTest/UnitTest1.cs
+++ SearchAmount/SearchAmountTest/UnitTest1.cs
@@ -1,15 +1,60 @@
+using NSubstitute;
+using SearchAmount.Repository;
+using SearchAmount.Service;
+
 namespace SearchAmountTest;
 
 public class Tests
 {
+    IBudgetRepo _budgetRepo;
+    private BudgetService _budgetService;
+
     [SetUp]
     public void Setup()
     {
+        _budgetRepo = Substitute.For<IBudgetRepo>();
+        _budgetService = new BudgetService(_budgetRepo);
     }
 
     [Test]
     public void Test1()
     {
         Assert.Pass();
+    }
+
+    [Test]
+    public void OneFullMonthSearch()
+    {
+        var start = new DateTime(2024, 1, 1);
+        var end = new DateTime(2024, 1, 31);
+        var expect = 310m;
+        _budgetRepo.GetAll().Returns(new List<Budget>()
+        {
+            new Budget()
+            {
+                YearMonth = "202401",
+                Amount = 310
+            }
+        });
+        var act = _budgetService.Query(start, end);
+        Assert.That(act, Is.EqualTo(expect));
+    }
+
+    [Test]
+    public void OneDaySearch()
+    {
+        var start = new DateTime(2024, 1, 1);
+        var end = new DateTime(2024, 1, 1);
+        var expect = 20m;
+        _budgetRepo.GetAll().Returns(new List<Budget>()
+        {
+            new Budget()
+            {
+                YearMonth = "202401",
+                Amount = 620
+            }
+        });
+        var act = _budgetService.Query(start, end);
+        Assert.That(act, Is.EqualTo(expect));
     }
 }
